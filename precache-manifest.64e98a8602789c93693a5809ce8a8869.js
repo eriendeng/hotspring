1ad71fbd7fc92d9de599b8317aa8443dcff8eda3
erienniu <97516719@qq.com>
2019-03-23 22:05:53 +0800

    static page

---
+++ precache-manifest.64e98a8602789c93693a5809ce8a8869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fb004ad5813f679fa577edda3ff496a",
+    "url": "/static/media/20171225202307425.4fb004ad.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "97052e3a5509dd3cf534",
+    "url": "/static/js/main.b143da76.chunk.js"
+  },
+  {
+    "revision": "dc012ac24166e9ed258a",
+    "url": "/static/js/2.0d258afd.chunk.js"
+  },
+  {
+    "revision": "97052e3a5509dd3cf534",
+    "url": "/static/css/main.5a084de1.chunk.css"
+  },
+  {
+    "revision": "c60598c1ece827608d2cbe9f73710ee8",
+    "url": "/index.html"
+  }
+];
